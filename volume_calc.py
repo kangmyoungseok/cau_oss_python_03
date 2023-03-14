008b97e74c08aa6a1109c8ddc971375b2c4eeac7
Myeongseokkang <kms00129@naver.com>
2023-03-14 16:07:06 +0900

    부피 계산 코드 업로드

---
+++ volume_calc.py
@@ -0,0 +1,6 @@
+width = float(input("가로:")) # 가로 값 입력
+length = float(input("세로:")) # 세로 값 입력
+height = float(input("높이:")) # 높이 입력
+volume = width * length * height  # 부피 계산
+
+print(f"박스의 부피는 {volume}입니다") # 부피 출력
