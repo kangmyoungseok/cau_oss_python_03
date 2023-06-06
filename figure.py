50a5a83cfb990ca08e4e2b5dabd6c30905c2518a
Myeongseokkang <kms00129@naver.com>
2023-06-06 12:38:35 +0900

    주석 형식 변경

--- figure.py
+++ figure.py
@@ -6,39 +6,82 @@ width 와 height 값을 가지는 클래스 line과
 import math
 
 class line:
-    '''넓이(__width)와 높이(__height)변수 존재'''
+    """line class
+    선들의 길이를 저장하는 클래스
+
+    Attributes:
+        __width: 선의 넓이
+        __height: 선의 높이
+    
+    Methods:
+        get_length: 선의 넓이와 높이를 반환
+        set_length: 선의 넓이와 높이를 설정
+    """
     __width,__height = 0,0
 
     def __init__(self, __width, __height):
-        '''넓이(__width)와 높이(__height) 초기화'''
         self.__width = __width
         self.__height = __height
 
     def get_length(self):
-        '''넓이(__width)와 높이(__height) 반환'''
         return self.__width,self.__height
     
     def set_length(self,__width,__height):
-        '''길이(length)를 설정'''
         self.__width = __width
         self.__height = __height
 
 def check_value(width,height):
-    ''' width와 height 값이 0 이하일 경우 ValueError를 발생'''
+    """check_value
+    width와 height가 0보다 작거나 같으면 ValueError를 발생시킨다.
+
+    Args:
+        width: 넓이
+        height: 높이
+
+    Raises:
+        ValueError: width와 height가 0보다 작거나 같을 때 발생
+    """
     if width <= 0 or height <= 0:
         raise ValueError
 
 def area_rectangle(width, height):
-    ''' width와 height를 매개변수로 받아, width * height의 직사각형의 넓이를 반환한다.'''
+    """area_rectangle
+    width와 height를 매개변수로 받아, width * height의 직사각형의 넓이를 반환한다.
+
+    Args:
+        width: 넓이
+        height: 높이
+
+    Returns:
+        width * height: 직사각형의 넓이        
+    """
     check_value(width,height)
     return width * height
 
 def area_ellipse(width,height):
-    ''' width와 height를 매개변수로 받아, width * height * pi의 타원의 넓이를 반환한다.'''
+    """area_ellipse
+    width와 height를 매개변수로 받아, width * height * pi의 타원의 넓이를 반환한다.
+
+    Args:
+        width: 넓이
+        height: 높이
+
+    Returns:
+        width * height * pi: 타원의 넓이
+    """
     check_value(width,height)
     return width * height * math.pi
 
 def area_right_triangle(width,height):
-    ''' width와 height를 매개변수로 받아, width * height / 2의 직각삼각형의 넓이를 반환한다.'''
+    """area_right_triangle
+    width와 height를 매개변수로 받아, width * height / 2의 직각삼각형의 넓이를 반환한다.
+
+    Args:
+        width: 넓이
+        height: 높이
+    
+    Returns:
+        width * height / 2: 직각삼각형의 넓이
+    """
     check_value(width,height)
     return width * height / 2
